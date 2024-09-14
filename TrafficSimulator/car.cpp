20b9fbc411afaf4b9712f816e34cd7a22ec5b8e2
quinngifford <155132289+quinngifford@users.noreply.github.com>
2024-09-14 06:26:47 -0700

    vers 2

--- TrafficSimulator/car.cpp
+++ TrafficSimulator/car.cpp
@@ -13,21 +13,12 @@
 #define max_turn 300.0f
 #define stop_gap 10.0f
 
-#define braking_distance_car 20.0f
-#define braking_distance_light 40.0f
 #define slowdown_distance_car 20.0f
 #define slowdown_distance_light 60.0f
 
 #define acceleration_rate 0.3f
-#define throttle_rate 0.2f
-#define brake_rate 0.2f
-#define fast_brake_rate 0.4f
-#define emergency_brake_rate 1.2f
-//#define max_steering_rate 0.8f
 
-//TO MAINTAIN VELOCITY AT 20: VEL = 20, THROTTLE = 2.1
-//TO MAINTAIN VELOCITY AT 30: VEL = 30, THROTTLE = 3.1
-//AND SO ON
+
 
 
 void Car::setPosition(float x, float y) {
@@ -64,7 +55,6 @@ void Car::addRotation(float angle, int wheel) {
     VBOC->Update(carVertices, sizeof(carVertices));
 }
 
-// Constructor definition
 Car::Car(float x, float y, int id) : lane(id) {
     setPosition(x, y);
     VBOC = new VBO(carVertices, sizeof(carVertices));
@@ -86,31 +76,43 @@ void Car::moveCar(float dt) {
 
     VBOC->Update(carVertices, sizeof(carVertices));
 
+    //if we havent enterned intersection
     if (turnStatus == 0) {
+        //if light is green
         if (lanes[lane].light == 0) {
+            //if car in front
             if (inFront != nullptr) {
                 followCar(dt);
             }
         }
+        //if light is red
         else if (lanes[lane].light == 2) {
+            //if red and car in front
             if (inFront != nullptr) {
+                //if red and car in front hasn't entered intersection
                 if (inFront->turnStatus == 0) {
                     stopForCar(dt);
                 }
+                //if red and car in front has entered intersection
                 else {
                     stopForLight(dt);
                 }
             }
+            //if red and no car in front
             else {
                 stopForLight(dt);
             }
         }
+        //if light is yellow
         else if (lanes[lane].light == 1) {
+            //if car in front
             if (inFront != nullptr) {
+                //if I'm making the light
                 if (makingLight()) {
                     followCar(dt);
                 }
             }
+            //if no car in front and I'm not making the light
             else if (!makingLight()) {
                 stopForLight(dt);
             }
@@ -137,38 +139,7 @@ void Car::moveCar(float dt) {
                 turnStatus = 2;
             }
         }
-    }
-#pragma region Steering Option
-    /*
-    * if (steering != 0) {
-        if (steering > 0) {
-            addRotation(steering * max_turn * dt, 18);
-        }
-        else {
-            addRotation(steering * max_turn * dt, 0);
-        }
-    }
-
-    if (turning = true) {
-        if (steering < steeringTarget) {
-            steering += steeringRate * dt * max_steering_rate;
-            if (steering >= steeringTarget) {
-                steering = steeringTarget;
-                turning = false;
-            }
-        }
-        else {
-            steering -= steeringRate * dt * max_steering_rate;
-            if (steering <= steeringTarget) {
-                steering = steeringTarget;
-                steering = false;
-            }
-        }
-    }
-    */
-#pragma endregion
-
-    
+    } 
 }
 
 void Car::setRotationTarget(float target, float rate) {
@@ -181,12 +152,12 @@ void Car::setRotationTarget(float target, float rate) {
 
 void Car::turnLeft() {
     rotationTarget = rotation + 90;
-    rotationRate = 0.2f;
+    rotationRate = 0.2f*(1+velocity*0.01);
     turnStatus = 1;
 }
 void Car::turnRight() {
     rotationTarget = rotation - 90;
-    rotationRate = 0.8f;
+    rotationRate = 0.8f*(1+velocity*0.01);
     turnStatus = 1;
 }
 
@@ -200,18 +171,17 @@ bool Car::makingLight() {
 }
 
 void Car::stopForCar(float dt) {
-    int count = 1;
+    int count = 0;
     Car* curr = lanes[lane].frontCar;
-    while (curr->next != this) {
-        count++;
+    while (curr != this) {
+        if (curr->turnStatus == 0) {
+            count++;
+        }
         curr = curr->next;
     }
     float dist_ = calculateDistance(carVertices[0], carVertices[1], lanes[lane].lightx, lanes[lane].lighty) - stop_gap;
     float dist = dist_ - (count * 10);
     if (dist < 0) {
-        std::cout << count << " ";
-        std::cout << dist_ << " ";
-        std::cout << dist << "\n";
         return;
     }
     if (dist < 60) {
@@ -226,7 +196,11 @@ void Car::stopForLight(float dt) {
     
 }
 void Car::followCar(float dt) {
-    if (inFront->velocity > velocity) {
+    float dist = calculateDistance(carVertices[0], carVertices[1], inFront->carVertices[0], inFront->carVertices[1]) - stop_gap;
+    if (dist < 23 && inFront->velocity < velocity) {
+        acceleration -= acceleration_rate * dt;
+    }
+    else {
         if (velocity < targetVelocity) {
             acceleration += acceleration_rate * dt;
         }
@@ -234,26 +208,7 @@ void Car::followCar(float dt) {
             acceleration = 0;
         }
     }
-    else {
-        float dist = calculateDistance(carVertices[0], carVertices[1], inFront->carVertices[0], inFront->carVertices[1]) - stop_gap;
-        if (dist < 40) {
-            acceleration -= acceleration_rate * dt;
-        }
-    }
 }
-
-#pragma region steering function
-/*
-void Car::setSteeringTarget(float target, float rate) {
-    steeringTarget = target;
-    steeringRate = rate;
-    if (target != steering) {
-        turning = true;
-    }
-}
-*/
-#pragma endregion
-
 
 
 
